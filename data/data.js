516a73336c3a111668a23ea7bfe07c191a43b5c8
Kegan Myers <kegan@keganmyers.com>
2014-04-14 17:58:49 -0500

    Update data, better moment UTC handling

--- data/data.js
+++ data/data.js
@@ -1,6 +1,6 @@
 window["locations"] = {
   stlCentralLibrary: {
-    timeZone: "-0600"
+    timeZone: "America/Chicago"
   }
 };
 
@@ -37,42 +37,42 @@ window["calendarData"] = [
     launchcodeTv: "http://tv.launchcode.us/#/videos/licensing-and-databases?lesson=MiniLectures"
   },
   {
-    date: "04-09-2014",
+    date: "04-09-2014 05:30PM",
     description: "Databases 101, SQL and NoSQL (relational vs. non-relational databases); and MongoDB",
     title: "Databases: SQL and NoSQL",
     location: "stlCentralLibrary",
     teacher: ['mike', 'kegan']
   },
   {
-    date: "04-14-2014",
+    date: "04-14-2014 05:30PM",
     description: "Front end (zen CSS, jQuery, AJAX, Bootstrap)",
     title: "Front End",
     location: "stlCentralLibrary",
     teacher: ['scott']
   },
   {
-    date: "04-16-2014",
+    date: "04-16-2014 05:30PM",
     description: "Object oriented programming, control flow (via Java)",
     title: "Object-Oriented via Space Invaders",
     location: "stlCentralLibrary",
     teacher: ['ricky']
   },
   {
-    date: "04-21-2014",
+    date: "04-21-2014 05:30PM",
     description: "Programming Languages Survey, with some data structures, Intro to Java/Spring, Intro to Ruby/Rails, More Javascript",
     title: "Programming languages with frameworks",
     location: "stlCentralLibrary",
     teacher: ['mike', 'ricky', 'kegan']
   },
   {
-    date: "04-23-2014",
+    date: "04-23-2014 05:30PM",
     description: "Deploying/packaging projects, heroku, travis",
     title: "Going live",
     location: "stlCentralLibrary",
     teacher: ['mike', 'ricky', 'kegan']
   },
   {
-    date: "04-28-2014",
+    date: "04-28-2014 05:30PM",
     description: "Interview tips/help",
     title: "Interview tips/help",
     location: "stlCentralLibrary"
